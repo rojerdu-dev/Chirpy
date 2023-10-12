f624046ae3f0226a2c5971f3a780f006eeaabbd9
Rojer Du <rojerdu.dev@gmail.com>
2023-10-11 19:03:51 -0700

    Create user.go and separate Chirp functionality into chirps.go

---
+++ internal/users.go
@@ -0,0 +1,41 @@
+package database
+
+type User struct {
+	ID    int    `json:"id"`
+	Email string `json:"email"`
+}
+
+func (db *DB) CreateUser(email string) (User, error) {
+	dbStructure, err := db.loadDB()
+	if err != nil {
+		return User{}, err
+	}
+
+	id := len(dbStructure.Users) + 1
+	user := User{
+		ID: id,
+		Email: email
+	}
+	dbStructure.Users[id] = user
+
+	err = db.writeDB(dbStructure)
+	if err != nil {
+		return User{}, err
+	}
+
+	return user, nil
+}
+
+func (db *DB) GetUser(id int) (User, error) {
+	dbStructure, err := db.loadDB()
+	if err != nil {
+		return User{}, err
+	}
+
+	user, ok := dbStructure.Users[id]
+	if !ok {
+		return User{}, err
+	}
+
+	return user, nil
+}
