268f3382bce88d34385518ea544839b36e6dbdd0
Rojer Du <rojerdu.dev@gmail.com>
2023-10-13 15:05:59 -0700

    Add MakeJWT, ValidateJWT, and GetBearerToken functions

---
+++ handler_users_update.go
@@ -0,0 +1,24 @@
+package main
+
+import "net/http"
+
+func (cfg *apiConfig) handlerUsersUpdate(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Password string `json:"password"`
+		Email    string `json:"email"`
+	}
+	type response struct {
+		User
+	}
+
+	token, err := auth.GetBearerToken(r.Header)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't find JWT")
+		return
+	}
+	subject, err := auth.ValidateJWT(token, cfg.jwtSecret)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't validate JWT")
+		return
+	}
+}
